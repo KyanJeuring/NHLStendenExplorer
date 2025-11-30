e4abc1cfe7b6ad8fcd64d5d616954fdbd1461359
StefanBryda <stefan.bryda@student.nhlstenden.com>
2025-11-30 17:01:20 +0100

    Finished footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,22 @@ import './footer.css';
 export default function Footer(){
     return (
         <footer>
-            hello word
+            <div class="footerContainer">
+                <div class="footerElement">
+                    Contact details <br/> <br/>
+                    Tel: <u>+31 58 244 1155</u> <br/>
+                    Email: <u>studentinfo@nhlstenden.com</u>
+                </div>
+            
+                <div class="footerElement">
+                
+                    Opening hours <br/> <br/>
+                    Mon-Thu 8:00-18:00 <br/>
+                    Fri 8:00-17:00
+                </div>
+            </div>
+            
+            
         </footer>
     )
 }
