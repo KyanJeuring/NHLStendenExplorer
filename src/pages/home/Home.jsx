e11a74e0cbd5f1ef65f279152c4b28b11c000576
Victoria Iascevschi <victoria.iascevschi@student.nhlstenden.com>
2025-12-03 19:40:23 +0100

    Fixed details on Home Page
    Modified button, did more space between button and section with hidden gems

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -14,8 +14,8 @@ export default function Home() {
             <p className="postalCode">Postal Code: 7811 KL Emmen</p>
             <p className="description">The Emmen branch is the second-largest branch after Leeuwarden, with a large, centrally located campus. Emmen offers a range of <u className="link"><a href="https://www.nhlstenden.com/en/courses?f%5B0%5D=degree-level%3A28&f%5B1%5D=location%3A3">Associate</a></u> and <u className="link"><a href="https://www.nhlstenden.com/en/courses?f%5B0%5D=degree-level%3A27&f%5B1%5D=location%3A3">Bachelor</a></u> degrees. The campus facilities have been completely refurbished and include a media centre, a library, a cafeteria and a pool table.</p>
             <img src="/photos/campusEmmenPhoto.webp" alt="NHL Stenden Emmen Campus" className="campusImage"/>
-            <Link to="/page1" className="buttonPulse">Start Campus Tour</Link>
-            <p className="explanationToButton">Explore our campus at your own pace. Discover facilities, study programs, and key locations. </p>
+            <Link to="/page1" className="buttonPulse"><svg className="playArrow"  viewBox="0 0 24 24"><path d="M8 19V5L19 12L8 19Z" /></svg>Start Campus Tour</Link>
+            <p className="explanationToButton">Explore our campus at your own pace. Discover facilities, study programs, and key locations.</p>
             <div className="pixelsCombinationForHomePage">
                 <img src="/pixels/combinationForHomePage.svg" alt="Pixels Combination" className="pixelsCombination"/>
                 <img src="/pixels/triangles.svg" alt="Pixel Triangles" className="pixelTriangles"/>
