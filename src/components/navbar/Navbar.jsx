b4b02c853eba8e79457eee98736e2a8bb61c52a3
FlaviusPetrasciuc <flavius.petrasciuc@student.nhlstenden.com>
2025-12-04 21:46:40 +0100

    Added media queries for phone, tablet and desktop for the nav bar and started the implementation of a hamburger menu component to be incorporated into the nav bar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import './navbar.css';  
+import BurgerMenu from '../hamburger_menu/burgermenu';
 
 export default function Navbar() {
+
+    const [burger_class, setBurgerClass] = useState("burger-bar unclicked")
+    const [menu_class, setMenuClass] = useState("menu hidden")
+    const [isMenuClicked, setIsMenuClicked] = useState(false) 
+
+    const updateMenu = () => {
+        if (!isMenuClicked) {
+            setBurgerClass("burger-bar clicked")
+            setMenuClass("menu visible")
+        } else {
+            setBurgerClass("burger-bar unclicked")
+            setMenuClass("menu hidden")
+        }
+    }
+
     return (
         <nav>
             <section className='logo'>
@@ -13,6 +29,9 @@ export default function Navbar() {
                 <NavLink to="/faq" className={({isActive}) => isActive ? 'nav-link active' : 'nav-link'}>FAQ</NavLink>
                 <NavLink to="/learnMore" className={({isActive}) => isActive ? 'nav-link active' : 'nav-link'}>Learn More</NavLink>
             </section>
+           <div className="hamburger-menu">
+                <BurgerMenu />
+            </div>
         </nav>
     );
 }
