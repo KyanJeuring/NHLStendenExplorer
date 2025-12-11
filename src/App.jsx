939ef1f8e8440803900a7827f0024f0f08890866
Kyan Jeuring <kyanjeuring@gmail.com>
2025-12-11 13:08:26 +0100

    Fixed app.jsx page render

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Home from './pages/home/Home'
 import Explore from './pages/explore/Explore'
 import Faq from './pages/faq/Faq'
 import Navbar from './components/navbar/Navbar'
-import Faq from './pages/faq/Faq'
 import LearnMore from './pages/learnMore/LearnMore'
 
 export default function App() {
@@ -16,7 +15,6 @@ export default function App() {
           <Route path="/" element={<Home/>}/>
           <Route path="/faq" element={<Faq/>}/>
           <Route path="/learnMore" element={<LearnMore/>}/>
-          <Route path="/faq" element={<Faq/>}/>
           <Route path="/explore" element={<Explore/>}/>
         </Routes>
       </main>
