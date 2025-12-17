9fdd11653dd074308bacece3dd3ef47782d24feb
StefanBryda <stefan.bryda@student.nhlstenden.com>
2025-12-17 17:09:40 +0100

    Added acnteen path to App

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/home/Home'
 import Navbar from './components/navbar/Navbar'
 import LearnMore from './pages/learnMore/LearnMore'
+import Canteen from './pages/canteen/Canteen'
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/learnMore" element={<LearnMore/>}/>
+          <Route path="/canteen" element={<Canteen/>}/>
         </Routes>
       </main>
     </>
