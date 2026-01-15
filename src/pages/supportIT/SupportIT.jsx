a970fb8964766310659f86aceccff622b23fc91b
Kyan Jeuring <kyanjeuring@gmail.com>
2026-01-15 21:58:27 +0100

    Added translation for SuportIT

--- src/pages/supportIT/SupportIT.jsx
+++ src/pages/supportIT/SupportIT.jsx
@@ -6,17 +6,13 @@ import TourGuide from '../../components/tourguide/TourGuide';
 const translations = {
     en: {
         title: "IT Support Service",
-        subtitle: "Having problems with your computer? Try seaking help here!",
-        bodyP1: "At our IT support service you can find help regarding your computer, as well as tools and programs you will need during your studies in our university. Just stop by at any moment and someone will help you solve your problem.",
-        moveNext: "Move Next"
+        subtitle: "Having problems with your computer? Try seeking help here!",
+        bodyP1: "At our IT support service you can find help regarding your computer, as well as tools and programs you will need during your studies in our university. Just stop by at any moment and someone will help you solve your problem."
     },
     nl: {
-        title: "Parkeren",
-        subtitle: "Op zoek naar een plek om je auto, fiets of scooter te parkeren?",
-        bodyP1: "Bij NHL Stenden Emmen is parkeren helemaal gratis. Er zijn veel parkeerplaatsen beschikbaar voor auto's, fietsen en zelfs scooters!. Het hoofdparkeerterrein, gelegen net buiten de Kennispoort, is de perfecte plek om je auto te parkeren. Maar wees voorzichtig! Parkeren op het gras is verboden en zal resulteren in wielklemmen!",
-        bodyP2: "Het parkeren van je auto op het gras is niet het enige dat verboden is. NHL Stenden is officieel een 'rookvrije' campus, wat betekent dat roken op het universiteitsterrein strikt verboden is. Het autoparkeerterrein is ook de plek om naartoe te gaan in geval van noodontruiming. Let gewoon op het langzame whoop-geluid alarm!",
-        bodyP3: "Het parkeerterrein voor fietsen en scooters is te vinden aan de andere kant van het gebouw, aan de Van Schaikweg. En maak je geen zorgen, er is genoeg ruimte voor iedereen! Het fietsparkeerterrein is overdekt, waardoor je fiets veilig en beschermd blijft tegen de regen.",
-        moveNext: "Ga verder"
+        title: "IT Ondersteuningsdienst",
+        subtitle: "Heb je problemen met je computer? Probeer hier hulp te zoeken!",
+        bodyP1: "Bij onze IT-ondersteuningsdienst kun je hulp vinden met betrekking tot je computer, evenals tools en programma's die je nodig hebt tijdens je studie aan onze universiteit. Loop gewoon op elk moment binnen en iemand zal je helpen je probleem op te lossen."
     }
 };
 
@@ -37,9 +33,6 @@ export default function SupportIT({ lang, toggleLang }) {
                 <p>{translate.bodyP1}</p>
                 <TourGuide lang={lang} />
             </main>
-            <div className="buttonToNextPage">                
-                <button onClick={() => navigate('/learnMore')} className="moveNext">{translate.moveNext}</button>
-            </div>
         </section>
     )
 }
